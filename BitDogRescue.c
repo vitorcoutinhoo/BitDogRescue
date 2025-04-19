6d572e81f8341a7ef43a6ff306a19c096256c4b7
Vitor <vitorcooutinho@gmail.com>
2025-04-19 16:44:26 -0300

    feat: add random position

--- BitDogRescue.c
+++ BitDogRescue.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "ssd1306.h"
@@ -18,7 +20,29 @@
 ssd1306_t ssd;
 
 // Variavel do Cronômetro
-char time[4];
+char timer[4];
+
+// Constantes de quantidade e tamanho
+#define MAX_VITIMAS 5
+#define DRONE_SIZE 8
+#define VITIMA_SIZE 4
+
+// Posição x e y das vitimas
+int posx[MAX_VITIMAS];
+int posy[MAX_VITIMAS];
+
+// Função pra atualizar o cronômetro
+bool update_timer(int, int);
+
+// Função pra desenhar o drone ou a vitima
+void draw_object(int x, int y, int size);
+
+// Gera as coordenadas aleatórias das vitimas
+void gerar_vitimas();
+
+// Desenha as vitimas no display
+void desenhar_vitimas();
+
 
 int main()
 {
@@ -45,27 +69,68 @@ int main()
     ssd1306_config(&ssd);
     ssd1306_send_data(&ssd);
 
+    gerar_vitimas();
+
     // Cronômetro
-    int count = 90;
+    int count = 0;
+    // posição do cronometro
+    int x = 0;
 
     while (true) {
-        sniprintf(time, sizeof(time), "%d", count);
-        ssd1306_rect(&ssd, 1, 1, 127, 63, true, false);
+        snprintf(timer, sizeof(timer), "%d", count);
+        ssd1306_rect(&ssd, 0, 0, 128, 64, true, false);
 
-        // tempo de 2 min pra completar o desafio
-        if(count <= 120)
-            ssd1306_draw_string(&ssd, time, 56, 28);
-        else {
-            ssd1306_fill(&ssd, false);
-            ssd1306_rect(&ssd, 1, 1, 127, 63, true, false);
-            ssd1306_draw_string(&ssd, "Voce Perdeu", 20, 26);
-            gpio_put(RED, true);
-
-            // volta pra tela inicial
-        }
+        update_timer(count, x);
+        desenhar_vitimas();
         ssd1306_send_data(&ssd);
 
         sleep_ms(1000);
         count += 1;
+    }
+}
+
+bool update_timer(int tempo, int x) {
+    // Tempo de 2 min pra completar o desafio
+    if(tempo <= 120) {
+        // Ajeita a pos do cronômetro no display
+        if((int)(floor(log10(tempo)) + 1) == 1 || tempo == 0)
+            x = 119;
+        if ((int)(floor(log10(tempo) + 1)) == 2)
+            x = 111;
+        if((int)(floor(log10(tempo) + 1)) == 3)
+            x = 103;
+
+        ssd1306_draw_string(&ssd, timer, x, 2);
+    }
+    else {
+        ssd1306_fill(&ssd, false);
+        ssd1306_rect(&ssd, 1, 1, 127, 63, true, false);
+
+        // Informa que o jogador perdeu e liga o led vermelho
+        ssd1306_draw_string(&ssd, "Voce Perdeu", 20, 26);
+        gpio_put(RED, true);
+
+        return false;
+    }
+
+    return true;
+}
+
+void draw_object(int x, int y, int size) {
+    ssd1306_rect(&ssd, x, y, size, size, true, true);
+}
+
+void gerar_vitimas(){
+    srand(to_us_since_boot(get_absolute_time()));
+
+    for(int i = 0; i < MAX_VITIMAS; i++){
+        posx[i] = (rand() % (WIDTH / VITIMA_SIZE)) * VITIMA_SIZE;
+        posy[i] = (rand() % (HEIGHT / VITIMA_SIZE)) * VITIMA_SIZE;
+    }
+}
+
+void desenhar_vitimas() {
+    for (int i = 0; i < MAX_VITIMAS; i++) {
+        draw_object(posx[i], posy[i], VITIMA_SIZE);
     }
 }
